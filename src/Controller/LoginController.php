1dc30d431185b73a7761ad8b5a2c1bdcbaab69a5
Theo QUEZEL PERRON <explo8413@gmail.com>
2023-03-05 13:58:22 +0100

    partial implement login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    public function __construct(
+        private readonly AuthenticationUtils $authenticationUtils,
+        private readonly Utilisateur $utilisateur,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly Request $request
+    )
+    {
+    }
+
     #[Route('/login', name: 'app_login')]
     public function index(): Response
     {
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'error' => $error,
+            'last_username' => $lastUsername
         ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        $user = new $this->utilisateur();
+
+        $this->request->get
+
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
