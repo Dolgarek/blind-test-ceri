9d4aa4a7cfbd1a0cf5b6f6cf547cbff559a2e391
EL Houari <bouchra.el-houari@alumni.univ-avignon.fr>
2023-03-06 13:50:53 +0100

    FEAT: L'ajout de la page de connection et d'inscription

---
+++ assets/react/controllers/PageInscriptionComponent.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+function RegistrationForm() {
+  const [name, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Nom:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('lastName:', password);
+    // Send data to server or do any other processing here
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nom:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Prénom:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Mot de passe:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <button type="submit">S'inscrire</button>
+
+    </form>
+  );
+}
+
+export default RegistrationForm;
---
+++ assets/react/controllers/PageLoginComponent.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Vérifiez les informations de connexion ici
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Nom d'utilisateur:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+
+      <label htmlFor="password">Mot de passe:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button type="submit">Se connecter</button>
+    </form>
+  );
+}
+
+export default LoginForm;
