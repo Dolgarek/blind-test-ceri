44b9ebcfe31a2f07ec2bfbcc2f44d0eb55d6674a
Marie Zephir <mz@efidem.com>
2023-04-03 22:03:09 +0200

    FIX - Implementing the database

---
+++ assets/react/controllers/PageJeuComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+export default function (props) {
+    return (
+        <div className='imageBackground'>
+            <div className='containerAccueil'>
+            
+            </div>
+        </div>
+    )
+}
